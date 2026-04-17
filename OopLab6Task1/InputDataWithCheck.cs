c22e1ebc167e1d44309600563e57095cb352c1d0
DANCOOL1337 <kozind311@gmail.com>
2026-04-17 09:35:53 +0500

    Update InputDataWithCheck.cs

--- OopLab6Task1/InputDataWithCheck.cs
+++ OopLab6Task1/InputDataWithCheck.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,22 +6,28 @@ namespace OopLab6Task1
 {
     class InputDataWithCheck
     {
-        // Метод для безопасного ввода логических значений (true/false)
-        static public bool InputBooleanWithValidation(string message)
+        
+        static public bool 
+            InputBooleanWithValidation(string message)
         {
             bool ok;
             bool result;
             do
             {
                 Console.WriteLine(message);
-                ok = bool.TryParse(Console.ReadLine(), out result);
+                ok = bool.TryParse(Console.ReadLine(), 
+                    out result);
 
                 if (!ok)
                 {
                     ConsoleColor tmp = Console.ForegroundColor;
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nВведенные данные имеют неверный формат.");
-                    Console.WriteLine("Ожидается ввод 'true' или 'false'. Повторите ввод.\n");
+                    Console.WriteLine
+                        ("\nВведенные данные имеют " +
+                        "неверный формат.");
+                    Console.WriteLine
+                        ("Ожидается ввод 'true' или 'false'. " +
+                        "Повторите ввод.\n");
                     Console.ForegroundColor = tmp;
                 }
             } while (!ok);
