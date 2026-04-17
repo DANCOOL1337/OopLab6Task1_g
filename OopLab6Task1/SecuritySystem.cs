a46266b8764096eab06229c25f01ad41949dab27
DANCOOL1337 <kozind311@gmail.com>
2026-04-17 09:33:23 +0500

    Update SecuritySystem.cs

--- OopLab6Task1/SecuritySystem.cs
+++ OopLab6Task1/SecuritySystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +6,7 @@ namespace OopLab6Task1
 {
     class SecuritySystem : LogicPair
     {
-        // Собственное поле: поставлена ли система на охрану
+        
         private bool isArmed;
 
         public bool IsArmed
@@ -15,19 +15,21 @@ namespace OopLab6Task1
             set { isArmed = value; }
         }
 
-        // Конструктор по умолчанию (вызывает базовый)
+        
         public SecuritySystem() : base()
         {
             this.isArmed = false;
         }
 
-        // Конструктор с параметрами (передает часть аргументов базовому)
-        public SecuritySystem(bool doorOpen, bool windowOpen, bool isArmed) : base(doorOpen, windowOpen)
+        /
+        public SecuritySystem
+            (bool doorOpen, bool windowOpen, 
+            bool isArmed) : base(doorOpen, windowOpen)
         {
             this.isArmed = isArmed;
         }
 
-        // Конструктор копирования
+        
         public SecuritySystem(SecuritySystem other) : base(other)
         {
             if (other != null)
@@ -36,28 +38,28 @@ namespace OopLab6Task1
             }
         }
 
-        // Метод 1: Проверка срабатывания тревоги
+        
         public bool CheckAlarmTrigger()
         {
-            // Если на охране и дизъюнкция (хоть что-то открыто) истинна.
-            // Так как CalculateNor() это !(A || B), если он вернул false - значит произошло вторжение.
+            
             if (this.isArmed && this.CalculateNor() == false)
             {
-                return true; // Тревога
+                return true; 
             }
-            return false; // Все спокойно
+            return false; 
         }
 
-        // Метод 2: Переключение статуса охраны
+        
         public void ToggleStatus()
         {
             this.isArmed = !this.isArmed;
         }
 
-        // Перегрузка ToString() с использованием base.ToString()
+        
         public override string ToString()
         {
-            return $"{base.ToString()}, На охране: {this.isArmed}";
+            return $"{base.ToString()}, " +
+                $"На охране: {this.isArmed}";
         }
     }
 }
