72f3dfa0556f6a444f2aaa13683703e613c4acc2
DANCOOL1337 <kozind311@gmail.com>
2026-04-21 12:20:41 +0500

    Update LineSegment.cs

--- OopLab6Task2/LineSegment.cs
+++ OopLab6Task2/LineSegment.cs
@@ -1,31 +1,15 @@
-using S_ystem;
+using System;
 
 namespace OopLab6
 {
     class LineSegment
     {
-        
+
         private double _x;
         private double _y;
 
-        
-        public double _x
-        {
-            get 
-                { return _x; }
-            set 
-                { _x = value; }
-        }
 
-        public double _y
-        {
-            get 
-                { return _y; }
-            set 
-                { _y = value; }
-        }
 
-        
         public LineSegment()
         {
             this._x = 0;
@@ -37,23 +21,46 @@ namespace OopLab6
             this._x = _x;
             this._y = _y;
         }
+        public double X
+        {
+            get
+            { 
+                return _x; 
+            }
+            set
+            {
+                _x = value; 
+            }
+        }
 
-        
+        public double Y
+        {
+            get
+            { 
+                return _y; 
+            }
+            set
+            { 
+                _y = value; 
+            }
+        }
+
+
         public LineSegment Intersect(LineSegment other)
         {
             if (other == null) return null;
 
             double s1 = Math.Min(this._x, this._y);
-            double e1 = Math.Ma_x(this._x, this._y);
+            double e1 = Math.Max(this._x, this._y);
             double s2 = Math.Min(other._x, other._y);
-            double e2 = Math.Ma_x(other._x, other._y);
+            double e2 = Math.Max(other._x, other._y);
 
-            double intersectStart = Math.Ma_x(s1, s2);
+            double intersectStart = Math.Max(s1, s2);
             double intersectEnd = Math.Min(e1, e2);
 
             if (intersectStart <= intersectEnd)
             {
-                return new 
+                return new
                     LineSegment(intersectStart, intersectEnd);
             }
             return null;
@@ -61,7 +68,7 @@ namespace OopLab6
 
         // --- (Задание 3) ---
 
-        
+
         public static LineSegment operator !(LineSegment s)
         {
             double new_x = s._x;
@@ -69,62 +76,65 @@ namespace OopLab6
 
             if (new_x > new_y)
             {
-               
+
 
                 if (0 <= new_y) new_x = 0;
-                
+
                 else if (new_x <= 0) new_y = 0;
-                
+
                 else { new_x = 0; new_y = 0; }
             }
             return new LineSegment(new_x, new_y);
         }
 
-        
+
         public static implicit operator int(LineSegment s)
         {
             return (int)s._y;
         }
 
-        
-        public static e_xplicit operator double(LineSegment s)
+
+        public static explicit operator double(LineSegment s)
         {
             return s._x;
         }
 
-        
-        public static LineSegment operator +(LineSegment s, 
+
+        public static LineSegment operator +(LineSegment s,
             int val)
         {
             LineSegment res = new LineSegment(s._x + val, s._y);
-            
-            if (res._x > res._y) { double t = res._x; res._x = 
-                    res._y; res._y = t; }
+
+            if (res._x > res._y)
+            {
+                double t = res._x; res._x =
+                    res._y; res._y = t;
+            }
             return res;
         }
 
-        
+
         public static LineSegment operator +(int val,
             LineSegment s)
         {
             LineSegment res = new LineSegment(s._x, s._y + val);
-            if (res._x > res._y) 
+            if (res._x > res._y)
             { double t = res._x; res._x = res._y; res._y = t; }
             return res;
         }
 
-        
+
         public static bool operator >(LineSegment left,
             LineSegment right)
         {
             double lMin = Math.Min(left._x, left._y);
-            double lMa_x = Math.Ma_x(left._x, left._y);
+            double lMa_x = Math.Max(left._x, left._y);
             double rMin = Math.Min(right._x, right._y);
-            double rMa_x = Math.Ma_x(right._x, right._y);
+            double rMa_x = Math.Max(right._x, right._y);
             return (lMin <= rMin) && (lMa_x >= rMa_x);
         }
 
-        
+
         public static bool operator <(LineSegment left,
             LineSegment right)
         {
